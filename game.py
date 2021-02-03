fe411f9420891cf4549963709d5ebdd8eee98686
victoriaruesch <vr365@georgetown.edu>
2021-02-03 17:55:45 -0500

    Generate Computer Choice

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 # game.py
 
-
+import random
 
 print("-------------------")
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
@@ -11,21 +11,34 @@ print("-------------------")
 #
 
 
-x = input("Please choose either 'rock', 'paper', or 'scissors':")
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors':")
 
 #print(x)
 #printing many things separated by commas
 #print ("You chose:", x, "another string", "something else", x)
 #print("You chose: ", x)
 
-
 #string concat:
 #print("You chose: " + x)
 
 #string interpolation / format string usage
-print(f"You Chose:{x}")
+print(f"You Chose: {user_choice}")
 
 #print("You chose: " + x)
+
+#
+# simulating a computer input
+#
+
+
+options = ("paper", "rock" , "scissors")
+computer_choice = random.choice(options)
+
+print(f"The Computer Chose: {computer_choice}")
+
+
+
+
 
 
 
@@ -36,23 +49,6 @@ print(f"You Chose:{x}")
 
 
 exit()
-
-
-
-
-
-
-
-
-
-
-
-#
-# simulating a computer input
-#
-
-print("The computer chose: 'paper'")
-
 
 print("-------------------")
 
